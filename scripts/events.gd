2029d2b04c549ced2a72535ca7adea9c85cb5e51
Sarah Schulte <git@sarah.engineer>
2025-01-30 08:27:43 -0800

    yayaya

--- scripts/events.gd
+++ scripts/events.gd
@@ -7,6 +7,8 @@ signal meter_calm
 var score: int = 0
 signal score_changed
 
+var charge = 0.0
+
 func add_score(diff: int):
 	score += diff
 	score_changed.emit()
--- scripts/fades.gd
+++ scripts/fades.gd
@@ -3,7 +3,41 @@ extends MeshInstance3D
 
 enum Electric {
 	GENERIC,
-	CEILING_FAN
+	CEILING_FAN,
+	CEILING_LIGHT,
+	TALL_LAMP,
+	SHORT_LAMP,
+	TV,
+	COMPUTER,
+	FRIDGE,
+	MICROWAVE,
+	OVEN,
+	DRYER,
+	WASHER,
+	CONSOLE,
+	RADIATOR,
+	SPEAKER,
+	TREADMILL,
+}
+
+var values = {
+	# points gained, min needed, pop threshold, max time
+	Electric.GENERIC: [50, 0, 100, 2.8],
+	Electric.CEILING_FAN: [50, 100, 1200, 1.0],
+	Electric.CEILING_LIGHT: [50, 0, 1000, 1.0],
+	Electric.TALL_LAMP: [50, 0, 100, 1.0],
+	Electric.SHORT_LAMP: [50, 0, 100, 1.0],
+	Electric.TV: [50, 0, 100, 2.8],
+	Electric.COMPUTER: [50, 0, 100, 2.8],
+	Electric.FRIDGE: [500, 500, 2000, 2.8],
+	Electric.MICROWAVE: [50, 0, 100, 2.8],
+	Electric.OVEN: [50, 0, 100, 2.8],
+	Electric.DRYER: [50, 0, 100, 2.8],
+	Electric.WASHER: [50, 0, 100, 2.8],
+	Electric.CONSOLE: [50, 0, 100, 2.8],
+	Electric.RADIATOR: [50, 0, 100, 2.8],
+	Electric.SPEAKER: [50, 0, 100, 2.8],
+	Electric.TREADMILL: [50, 0, 100, 2.8],
 }
 
 @export var type: Electric = Electric.GENERIC
@@ -12,9 +46,32 @@ enum Electric {
 @export var electrified = false
 @export var fades = false
 @export var solid_while_alive = false
+@export var mandatory = false
 var dead = false
 var mat: StandardMaterial3D
 
+func unlerp(from: float, to: float, value: float) -> float:
+	return (value - from) / (to - from)
+
+func is_possible():
+	var d = values[type]
+	return Events.score >= d[1]
+
+func get_max_charge_time():
+	var d = values[type]
+	return d[3]
+	
+func get_charge_time():
+	var d = values[type]
+	if Events.score >= d[2]:
+		return 0.0
+	if Events.score < d[1]:
+		var t = unlerp(0.0, d[1], max(Events.score, 10.0))
+		return d[3] / t
+	var t = unlerp(d[1], d[2], Events.score)
+	print("T: ", t)
+	return lerp(d[3], 0.2, t)
+	
 func kill():
 	#var anim = find_child("AnimationPlayer")
 	#if is_instance_valid(anim):
