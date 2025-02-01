11e80ec82987d61d8e35f86fd987fc8f9d2eab4f
Sarah Schulte <git@sarah.engineer>
2025-02-01 14:19:40 -0800

    blinky

--- scripts/guy.gd
+++ scripts/guy.gd
@@ -1,10 +1,4 @@
-extends AnimatedSprite3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	play("default")
-	pass # Replace with function body.
+extends Sprite3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
